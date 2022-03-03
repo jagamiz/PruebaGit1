3663513a35f16b6e2b6f815cb480901207ebe09a
jagamiz <jagamiz@iestrassierra.com>
2022-03-03 11:37:14 +0100

    Escribimos Hola Universo

--- src/pruebagit1/PruebaGit1.java
+++ src/pruebagit1/PruebaGit1.java
@@ -16,6 +16,7 @@ public class PruebaGit1 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola mundo");
+        System.out.println("Hola Universo");
     }
     
 }
