3a46e94d2c09dc337700d0491af4c65d2007910b
jagamiz <jagamiz@iestrassierra.com>
2022-03-03 11:39:03 +0100

    Revert "Escribimos Hola Universo"
    This reverts commit 3663513a35f16b6e2b6f815cb480901207ebe09a.

--- src/pruebagit1/PruebaGit1.java
+++ src/pruebagit1/PruebaGit1.java
@@ -16,7 +16,6 @@ public class PruebaGit1 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola mundo");
-        System.out.println("Hola Universo");
     }
     
 }
