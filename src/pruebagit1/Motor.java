96aa816335e076cae40edc0fc7f39862d1643b35
jagamiz <jagamiz@iestrassierra.com>
2022-03-10 19:29:51 +0100

    añadir motor

---
+++ src/pruebagit1/Motor.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package pruebagit1;
+
+/**
+ *
+ * @author igo 
+ */
+public class Motor {
+    int cilindrada;
+    int potencia;
+
+    public int getCilindrada() {
+        return cilindrada;
+    }
+
+    public void setCilindrada(int cilindrada) {
+        this.cilindrada = cilindrada;
+    }
+
+    public int getPotencia() {
+        return potencia;
+    }
+
+    public void setPotencia(int potencia) {
+        this.potencia = potencia;
+    }
+    
+}
